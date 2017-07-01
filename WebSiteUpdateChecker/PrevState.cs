07cdb2e0dabbf5b9bef8232b8e3130fb770bba9b
Max <grimfire@gmail.com>
2017-07-01 20:39:01 +0300

    update

---
+++ WebSiteUpdateChecker/PrevState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSiteUpdateChecker
+{
+    public class PrevState
+    {
+        public string Url { get; set; }
+        public string Element { get; set; }
+        public string RegexPattern { get; set; }
+    }
+}
